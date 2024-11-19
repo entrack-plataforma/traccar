81a93a7b8fe2e8f180f42c84c29b7207e85051f9
entrack-plataforma <entrack.plataforma@gmail.com>
2024-11-19 14:11:28 +0000

    Update TaipProtocolDecoder.java

--- src/main/java/org/traccar/protocol/TaipProtocolDecoder.java
+++ src/main/java/org/traccar/protocol/TaipProtocolDecoder.java
@@ -47,7 +47,7 @@ public class TaipProtocolDecoder extends BaseProtocolDecoder {
             .groupEnd("?")
             .number("(d{5})")                    // seconds
             .or()
-            .expression("(?:RGP|RCQ|RCV|RBR|RUS00|RPI),?") // type
+            .expression("(?:RGP|RCQ|RCV|RBR|RUS00|RUS0D|RPI),?") // type
             .number("(dd)?")                     // event
             .number("(dd)(dd)(dd)")              // date (mmddyy)
             .number("(dd)(dd)(dd)")              // time (hhmmss)
